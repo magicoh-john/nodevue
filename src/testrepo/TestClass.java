b64c4a628808d0479d1df3159be800e2dcc0c266
Oh Young Joo <magicoh7@gmail.com>
2021-03-17 10:29:24 +0900

    testrepo initialize

---
+++ src/testrepo/TestClass.java
@@ -0,0 +1,15 @@
+package testrepo;
+
+public class TestClass {
+
+	public TestClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello! This is Github practice");
+		//Github practice
+
+	}
+
+}
